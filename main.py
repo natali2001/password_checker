452b3859c0ade535e0ae9f7bc781f39b3d841cd4
natali2001 <49942113+natali2001@users.noreply.github.com>
2022-04-18 22:29:12 +0300

    Add files via upload
    Created conditions for checking password

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+
+password = input("Type your password:")
+k = 0
+
+if (len(password)<14):
+    print("Password must consist at least 14 characters.")
+    
+if not re.search("[a-z]", password):
+        print("Password must consist lower case symbols")
+        
+if not re.search("[A-Z]", password):
+        print("Password must consist upper case symbols")
+        
+if not re.search("[0-9]", password):
+        print("Password must consist digits")
+        
+if not re.search("[!?_@$]", password):
+        print("Password must consist punctuation numbers")
+else:
+        print("Valid Password")
+    
+
+
+
