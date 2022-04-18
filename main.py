cf3570961be612c2cfd949eb54706db827ff5f05
natali2001 <49942113+natali2001@users.noreply.github.com>
2022-04-18 23:54:52 +0300

    add some punctuation characters

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ password = input("Type your password:")
 k = 0
 
 if (len(password)<14):
-    print("Password must be at least 14 characters long.")
+    print("Password must consist at least 14 characters.")
     k = -1
     
-if not re.search("[a-z]+[A-Z]", password):
-        print("Password must contain both lowercase and uppercase characters")
+if not re.search("[a-z]", password):
+        print("Password must consist lower case symbols")
+        k = -1
+        
+if not re.search("[A-Z]", password):
+        print("Password must consist upper case symbols")
         k = -1
         
 if not re.search("[0-9]", password):
-        print("Password must contain digits")
+        print("Password must consist digits")
         k = -1
         
-if not re.search("[!?_@$]", password):
-        print("Password must contain at least one punctuation character")
+if not re.search("[!?_@#%()*+'-/,.:;<=>{|}^~$]", password):
+        print("Password must consist punctuation numbers")
         k = -1
 
 if (k>-1):
     print('Strong Password')
-    
+
+
+
 
 
 
