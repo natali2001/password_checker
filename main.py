6616c6cf97d16ce01174a168897b1b5c6eb4a623
natali2001 <49942113+natali2001@users.noreply.github.com>
2022-04-18 23:26:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,19 @@ password = input("Type your password:")
 k = 0
 
 if (len(password)<14):
-    print("Password must consist at least 14 characters.")
+    print("Password must be at least 14 characters long.")
     k = -1
     
-if not re.search("[a-z]", password):
-        print("Password must consist lower case symbols")
-        k = -1
-        
-if not re.search("[A-Z]", password):
-        print("Password must consist upper case symbols")
+if not re.search("[a-z]+[A-Z]", password):
+        print("Password must contain both lowercase and uppercase characters")
         k = -1
         
 if not re.search("[0-9]", password):
-        print("Password must consist digits")
+        print("Password must contain digits")
         k = -1
         
 if not re.search("[!?_@$]", password):
-        print("Password must consist punctuation numbers")
+        print("Password must contain at least one punctuation character")
         k = -1
 
 if (k>-1):
